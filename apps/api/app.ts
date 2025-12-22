7da7a07dc72a4d594a69f85784e0276ebc07ea93
Gitty Landau <gittylandau04@gmail.com>
2025-12-21 22:20:50 -0500

    api

---
+++ apps/api/app.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from 'express';
+import path from 'path';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the React app
+const distPath = path.join(process.cwd(), '../web/dist');
+
+app.use(express.static(distPath));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (_req: Request, res: Response) => {
+    res.sendFile(path.join(distPath, 'index.html'));
+});
+
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
